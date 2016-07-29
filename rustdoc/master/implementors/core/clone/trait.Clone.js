2810c2e1a2267a91a1dfb93f7c7c2af8f2a6935d
Stu Black <trurl@freeshell.org>
2016-07-28 23:35:33 -0400

    Update rustdocs for master.

--- rustdoc/master/implementors/core/clone/trait.Clone.js
+++ rustdoc/master/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["symbol_table"] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='symbol_table/indexing/struct.Ref.html' title='symbol_table::indexing::Ref'>Ref</a>&lt;T&gt;",];
+implementors["symbol_table"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='enum' href='symbol_table/indexing/enum.Insertion.html' title='symbol_table::indexing::Insertion'>Insertion</a>&lt;T&gt;","impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='symbol_table/indexing/struct.Ref.html' title='symbol_table::indexing::Ref'>Ref</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
