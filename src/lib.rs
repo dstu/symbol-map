d56232bb58668deb25b35bb3e74f4490bb08677f
Stu Black <trurl@freeshell.org>
2016-09-08 21:37:22 -0400

    Export TableIntoIter in crate root.

--- src/lib.rs
+++ src/lib.rs
@@ -55,4 +55,4 @@ mod table;  // Not pub because all pub symbols re-exported.
 
 #[cfg(test)] extern crate crossbeam;
 
-pub use self::table::{Symbol, SymbolId, Table, TableIter};
+pub use self::table::{Symbol, SymbolId, Table, TableIntoIter, TableIter};
